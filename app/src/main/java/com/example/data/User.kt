bb0041a4c0cd0d9c0a21d40cb1227151f2d347e0
Kántor Hunor-Ákos <kantorakos99@gmail.com>
2020-12-01 21:50:24 +0200

    Database and Recyclerview refinement

--- app/src/main/java/com/example/data/User.kt
+++ app/src/main/java/com/example/data/User.kt
@@ -4,7 +4,7 @@ import androidx.room.*
 
 @Entity
 data class User(
-    @PrimaryKey val uid: Int,
+    @PrimaryKey(autoGenerate = true) val uid: Int,
     @ColumnInfo(name = "first_name") val firstName: String?,
     @ColumnInfo(name = "last_name") val lastName: String?
 )
